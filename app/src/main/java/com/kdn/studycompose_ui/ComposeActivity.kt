e204cb469bd05a4a223461cb6a36ccc0e9805299
choiyunsung <12345ggh81@gmail.com>
2022-05-16 20:47:32 +0900

    [UPDATE] compose button add

--- app/src/main/java/com/kdn/studycompose_ui/ComposeActivity.kt
+++ app/src/main/java/com/kdn/studycompose_ui/ComposeActivity.kt
@@ -4,32 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kdn.studycompose_ui.ui.theme.StudyCompose_UITheme
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { Greeting2("Android") }
+        setContent { GreetingText("world") }
 
     }
 
     @Composable
-    fun Greeting2(name: String) {
-        Text(text = "Hello $name!")
+    fun GreetingText(name: String) {
+        Text(text = "Hello $name!", modifier = Modifier.size(width = 80.dp, height = 80.dp))
     }
+
+//    @Composable
+//    fun GreetingButton(name : String){
+//        Button(onClick = {}) {
+//            Greeting(name = "$name Button")
+//        }
+//    }
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview2() {
         StudyCompose_UITheme {
-            Greeting2("Android")
+           GreetingText(name = "world")
         }
     }
 }
