80a22b162f0220c0a9161067e51dcf08909d4798
choiyunsung <12345ggh81@gmail.com>
2022-05-20 08:54:41 +0900

    [UPDATE] Surface + Surface 즉, 부모 레이아웃에 Text 추가하기

--- app/src/main/java/com/kdn/studycompose_ui/ComposeActivity.kt
+++ app/src/main/java/com/kdn/studycompose_ui/ComposeActivity.kt
@@ -11,45 +11,50 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kdn.studycompose_ui.ui.theme.StudyCompose_UITheme
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { GreetingText("world") }
+        setContent { MainScreen() }
 
     }
 
-    // 순서에 따른 클릭 범위를 설정해줄 수 있다.
+    @Composable
+    fun MainScreen(){
+        Surface(
+            color = Color.Gray,
+            modifier = Modifier.fillMaxSize()) {
 
-    // padding 을 먼저 or clickable 을 먼저
-    // 크기를 먼저 지정할지, padding 을 먼저 지정해줄지.
+            Surface(
+                color = Color.Green,
+                modifier = Modifier.wrapContentSize()) {
+                Text(
+                    text = "Wrapped content",
+                    style = MaterialTheme.typography.h3)
+            }
+            
+        }
+    }
 
     @Composable
     fun GreetingText(name: String) {
-        Text(text = "Hello $name!",
-            modifier = Modifier
-                .height(240.dp)
-                .width(200.dp)
-                .padding(24.dp)
-                .clickable {  })
+        Text(text = "Hello $name!")
     }
-
-//    @Composable
-//    fun GreetingButton(name : String){
-//        Button(onClick = {}) {
-//            Greeting(name = "$name Button")
-//        }
-//    }
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview2() {
         StudyCompose_UITheme {
-           GreetingText(name = "world")
+            MainScreen()
         }
     }
 }
