ed3a8de6deda1f1f32cdfe88fb6798fff1c80ec9
choiyunsung <12345ggh81@gmail.com>
2022-05-25 23:19:20 +0900

    [UPDATE] 동적 UI 콘텐츠가 업데이트 되지 않는 상황 추가

--- app/src/main/java/com/kdn/studycompose_ui/DynamicContentExample.kt
+++ app/src/main/java/com/kdn/studycompose_ui/DynamicContentExample.kt
@@ -3,17 +3,20 @@ package com.kdn.studycompose_ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kdn.studycompose_ui.ui.theme.StudyCompose_UITheme
 
-private val namesList : List<String> = listOf(
+private val namesList : ArrayList<String> = arrayListOf(
     "john","Michael","Andrew","Danna","JOJI")
 
 class DynamicContentExample : ComponentActivity() {
@@ -26,19 +29,27 @@ class DynamicContentExample : ComponentActivity() {
 }
 
 @Composable
-fun GreetingList(names : List<String>){
-    Column() {
+fun GreetingList(names : ArrayList<String>){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
         for(name in names){
             Greeting2(name = name)
         }
         
-        Greeting2(name = names[0])
+        Button(onClick = {names.add("new name")}) {
+            Text(text = "Add new name")
+        }
     }
 }
 
 @Composable
 fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h4)
 }
 
 @Preview(showBackground = true)
